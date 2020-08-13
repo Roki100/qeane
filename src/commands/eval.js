6619f8873f2e794f9a14231a03ac22db552854ba
Lumap <lmp@lumap.localdomain>
2020-08-13 11:20:21 +0200

    ah

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -20,7 +20,7 @@ module.exports = {
         var evaled = await eval(msg.args.join(" "));
       }
       if (evaled.length > 2000) evaled = evaled.sliceEvery(2000)[0]
-      msg.reply(evaled, { code: "js" })
+      msg.reply(evaled)
 
     } catch (err) {
       console.log(err)
