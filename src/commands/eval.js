b20fe18f9c33c22096214a5ff123902004004fe2
Lumap <lmp@lumap.localdomain>
2020-08-17 16:00:55 +0200

    e

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -17,10 +17,12 @@ module.exports = {
       if (msg.args[0] === "inspect") {
         let evaled = require('util').inspect(await eval(msg.args.slice(1).join(" ")));
         if (evaled.length > 2000) evaled = evaled.sliceEvery(2000)[0]
+        if (evaled.includes(client.token)) evaled = evaled.replace(client.token, "no plz, dont leak token")
         msg.reply(`${msg.author.tag}: ` + evaled, { code: "js" })
       } else {
         let evaled = await eval(msg.args.join(" "));
         if (evaled.length > 2000) evaled = evaled.sliceEvery(2000)[0]
+        if (evaled.includes(client.token)) evaled = evaled.replace(client.token, "no plz, dont leak token")
         msg.reply(`${msg.author.tag}: ` + evaled)
       }
 
