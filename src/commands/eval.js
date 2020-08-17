da688b9a520de8fe2c8e3ae61cf283a077aeaf25
Lumap <lmp@lumap.localdomain>
2020-08-17 16:02:33 +0200

    e

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -1,28 +1,24 @@
-const dual = require('@tafina/dualfm2')
-
-const l = dual.listeners();
-const lp = dual.listeners_peak();
-const now = dual.nowplaying();
-const artist = dual.artist();
-
-const nowp = dual.nowplaying() + ' - ' + dual.artist()
-const bobby = "DJ Bobby - https://dualfm.net"
 module.exports = {
   name: 'eval',
   ownerOnly: true,
   category: "owner",
   async execute(client, msg) {
     let str = client.languages.get(msg.guild.language).commands.eval
+    if (!msg.args.join(" ")) return msg.reply(`${msg.author.tag}: ` + "plz send args")
     try {
       if (msg.args[0] === "inspect") {
         let evaled = require('util').inspect(await eval(msg.args.slice(1).join(" ")));
-        if (evaled.length > 2000) evaled = evaled.sliceEvery(2000)[0]
-        if (evaled.includes(client.token)) evaled = evaled.replace(client.token, "no plz, dont leak token")
+        if (evaled) {
+          if (evaled.length > 2000) evaled = evaled.sliceEvery(2000)[0]
+          if (evaled.includes(client.token)) evaled = evaled.replace(client.token, "no plz, dont leak token")
+        }
         msg.reply(`${msg.author.tag}: ` + evaled, { code: "js" })
       } else {
         let evaled = await eval(msg.args.join(" "));
-        if (evaled.length > 2000) evaled = evaled.sliceEvery(2000)[0]
-        if (evaled.includes(client.token)) evaled = evaled.replace(client.token, "no plz, dont leak token")
+        if (evaled) {
+          if (evaled.length > 2000) evaled = evaled.sliceEvery(2000)[0]
+          if (evaled.includes(client.token)) evaled = evaled.replace(client.token, "no plz, dont leak token")
+        }
         msg.reply(`${msg.author.tag}: ` + evaled)
       }
 
