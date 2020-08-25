034e65e6c849a28b2e829b0f885bae495539d081
lumap <lumapbk@gmail.com>
2020-08-25 18:08:07 +0200

    e

--- src/events/msg.js
+++ src/events/msg.js
@@ -1,6 +1,5 @@
 module.exports = async (client, msg) => {
   if (msg.content === '' || !msg.guild || msg.channel.type === "dm" || msg.author.bot || msg.webhookID) return;
-
   let prefix = client.db.get(`${msg.guild.id}.prefix`) || client.config.prefix
   if (msg.content === `<@!${client.user.id}>`) return msg.reply(`${msg.author.tag}: Hi! I am Qeane, a cool music bot with a lot of useful features! My prefix here is **${prefix}**, so type **${prefix}help** to get a command list!`)
   if (msg.content.startsWith(`<@!${client.user.id}>`)) prefix = `<@!${client.user.id}>`
--- src/functions/musicCheck.js
+++ src/functions/musicCheck.js
@@ -1,4 +1,4 @@
-module.exports = (client, msg) => {
+module.exports = async (client, msg) => {
     let serverQueue = client.queue.get(msg.guild.id)
     if (!serverQueue) return msg.reply(`${msg.author.tag}: Nothing is playing right now!`)
     if (!msg.member.voice.channel) return msg.reply(`${msg.author.tag}: You need to be connected to a voice channel to do that!`)
