267d842e55fe4b3d7fd2f4e5cdfd374be5dc684f
Roki <35699619+Roki100@users.noreply.github.com>
2020-09-30 23:09:44 +0200

    fix readme, add some semicolons and bump axios version

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# qeane
+# Qeane
 Qeane is one of the best open-source music bot ever! 
 # The official bot
 You can invite the official bot [here](https://discord.com/api/oauth2/authorize?client_id=727163097026003004&permissions=8&scope=bot)
 # Self-hosting
-In order for the bot to fully work, you'll need:<br />
-*npm and node.js 13.12.0 installed
-*a lavalink node (see how to make one [here](https://github.com/Frederikam/Lavalink))<br />
-*your bot token<br />
-*your user ID
+In order for the bot to fully work, you'll need:
+* npm and node.js 13.12.0 installed
+* a lavalink node (see how to make one [here](https://github.com/Frederikam/Lavalink))
+* your bot token
+* your user ID
 
 Just put everything in the config.example.json, rename it to config.json and voila!<br />
 If any problem occurs, please contact me. [My server discord](https://discord.gg/nXg4Yh7)
--- src/functions/musicCheck.js
+++ src/functions/musicCheck.js
@@ -1,7 +1,7 @@
 module.exports = async (client, msg) => {
     let serverQueue = client.queue.get(msg.guild.id)
-    if (!serverQueue) return msg.send(`${msg.author.tag}: Nothing is playing right now!`)
-    if (!msg.member.voice.channel) return msg.send(`${msg.author.tag}: You need to be connected to a voice channel to do that!`)
+    if (!serverQueue) return msg.send(`${msg.author.tag}: Nothing is playing right now!`);
+    if (!msg.member.voice.channel) return msg.send(`${msg.author.tag}: You need to be connected to a voice channel to do that!`);
     let vc = await msg.member.voice.channel.fetch()
-    if (serverQueue.voiceChannel.id !== vc.id) return msg.send(`${msg.author.tag}: You need to be in my voice channel to do that!`)
+    if (serverQueue.voiceChannel.id !== vc.id) return msg.send(`${msg.author.tag}: You need to be in my voice channel to do that!`);
 }
